78fc5c9e3956375423d4afc64cb2d3eba0d70756
Mark Eliasen <mark.eliasen@pm.me>
2024-09-11 13:07:30 +0100

    docs: make docs better

--- src/pkg/api/internal/endpoint_docs.go
+++ src/pkg/api/internal/endpoint_docs.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -12,17 +11,5 @@ import (
 var rng = utils.NewSeededRNG(time.Now().UnixMicro())
 
 func EndpointDocs(c *gin.Context) {
-	headers := []string{
-		"Method", "Endpoints", "Response Type", "Args", "Notes",
-	}
-	data := [][]string{
-		{"GET", "/api/galaxy", "JSON", "?seed=<number> (optional, galaxy seed)", "Generates a galaxy with a single stellar neighbourhood"},
-		{"GET", "/api/galaxy/<galaxy seed>/neighbourhood", "JSON", "?seed=<number> (optional, neighbourhood seed)", "Generates another stellar neighbourhood for the specified galaxy"},
-	}
-
-	table := utils.ToTable(headers, data)
-
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"table": strings.Join(table, "\n"),
-	})
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{})
 }
