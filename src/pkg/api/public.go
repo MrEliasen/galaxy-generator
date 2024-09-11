b88ad5195ee36f3c3358075b7c7e0dbc50e822fd
Mark Eliasen <mark.eliasen@pm.me>
2024-09-11 14:02:33 +0100

    fix: favicon missing

--- src/pkg/api/public.go
+++ src/pkg/api/public.go
@@ -3,6 +3,7 @@ package api
 import (
 	"embed"
 	"html/template"
+	"net/http"
 
 	g "github.com/CAFxX/httpcompression/contrib/gin-gonic/gin"
 	"github.com/gin-gonic/gin"
@@ -12,6 +13,9 @@ import (
 //go:embed internal/templates/*
 var templateFS embed.FS
 
+//go:embed internal/assets/images/favicon.ico
+var favicon []byte
+
 func Run() {
 	compressor, _ := g.DefaultAdapter()
 
@@ -20,6 +24,10 @@ func Run() {
 
 	tmpl := template.Must(template.New("").ParseFS(templateFS, "internal/templates/*"))
 	r.SetHTMLTemplate(tmpl)
+
+	r.GET("/favicon.ico", func(c *gin.Context) {
+		c.Data(http.StatusOK, "image/x-icon", favicon)
+	})
 
 	r.GET("/api/galaxy", internal.EndpointGalaxy)
 	r.GET("/api/galaxy/:seed/neighbourhood", internal.EndpointNeighbourhood)
